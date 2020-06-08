903d000a0745c8919a661fcda1e4c50795cd74e2
baduhai <baduhai@github.com>
2020-06-08 18:31:26 +0100

    Updated script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ScrapePie
-Simple website info scraper that sends Telegram notification when information on webpage changes. Supports loging in to websites.
+Simple website info scraper that sends Telegram notification when information on webpage changes. Supports logging in to websites.
+
+Read all comments on the script before running, as they also contain instructions.
--- scrape.py
+++ scrape.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 import time # Needed for sleep function
+import vlc # Play lound sounds
 import mechanize # Needed for browser functionality
 import http.cookiejar # Needed to save session cookies
 import ssl # Needed to managed ssl
 import html2text # Needed to parse html into something readable
 import subprocess # Interact with system commands
+from urllib.error import URLError, HTTPError # Error handling
 
 # Browser
 br = mechanize.Browser()
@@ -21,11 +23,19 @@ br.set_handle_redirect(True)
 br.set_handle_referer(True)
 br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-br.set_header = [('User-Agent', '<put a sane user-agent here>')]
+br.set_header = [('User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:76.0) Gecko/20100101 Firefox/76.0')]
 
+# Sound file location
+p = vlc.MediaPlayer('file:///home/william/Music/Sounds/siren.mp3')
+
+# May be useful if going through web browser's developer tools is too difficult for any reason.
 # View available forms (in order to know which form index to select)
 #for f in br.forms():
 #    print(f)
+
+# Misc stuff
+j = 0
+h = 0
 
 def login():
     # Disable ssl cause I don't know how to set it up
@@ -38,31 +48,57 @@ def login():
         # Handle target environment that doesn't support HTTPS verification
         ssl._create_default_https_context = _create_unverified_https_context
     # Session handling for website
-    br.open('login page address')
+    br.open('https://ec-lisboa.itamaraty.gov.br/login')
     # Select the index form for login
     br.select_form(nr=0)
     # User credentials
-    br.form['email'] = '<example@email.com>' # This field may be named differently depending on website
-    br.form['pass'] = '<password>' # This field may be named differently depending on website
-    # There may be a third field for authentication token that you have to find manually
+    # Form fileds may be named differently depending on website
+    br.form['email'] = ''
+    br.form['pass'] = ''
+    # There may be a third form field that you need to send, it is usually invisible to the user, use your web browser's dev tools to find what it's called and what to fill in the field before submitting
     # Login
     br.submit()
 
-# Main function 
 def check():
-    # What are we reading?
-    content = str(br.open('page address for where info will be').read())
-    parsedContent = str(html2text.html2text(content))
-    conca = parsedContent.splitlines()[77] + parsedContent.splitlines()[78] # Specific lines where you'll find changes
-    return conca    
+    try:
+        # What are we reading?
+        content = str(br.open('https://ec-lisboa.itamaraty.gov.br/availability').read())
+        parsedContent = str(html2text.html2text(content))
+        noBreaksParsedContent = parsedContent.replace("\n", "")
+        return noBreaksParsedContent
+    # Adding the two following error exceptions fixes(hopefully) any problems that may arrise due to connectivity, be it server-side or client-side.
+    except HTTPError as e:
+        print('Server couldn\'t fulfill request.')
+        print('Reason: ', e.reason, '. Restarting')
+        main()
+    except URLError as e:
+        print('Failed to reach server.')
+        print('Reason: ', e.reason, '. Restarting')
+        main()
 
-login() # Log ins only once, as session cookies are saved
-i = 0
-while True:
-    if len(check()) != 101: # Number of characters the relevant line of data has when there are no changes
-        subprocess.run(['telegram-send', 'Data changed! ']) # Send message via telegram (telegram-send must be installed and configured!)
-        break
-    else:
-        i += 1
-        print('Number of attempts: ' + str(i)) # Prints how many times it's attempted to find new info.
-        time.sleep(5) # Delay unti trying again
+# Main function 
+def main():
+    global j
+    global h
+    i = 0
+    login() # Seldom logs in, as session cookies are saved
+    while h <= 10:
+        toCheck = check()
+        if toCheck.find("Passaporte para homens (entre 18 e 45 anos) - solteiro| Indi") < 0:
+            h += 1
+            print('Horário disponível!!!')
+            subprocess.run(['telegram-send', 'Horário disponível, corre!'])
+            break
+        else:
+            j += 1
+            i += 1
+            print('Number of attempts: ' + str(j))
+            time.sleep(5)
+            if i >= 400: # value may need to be changed, monitor the script to see what value makes sense.
+                main()
+    p.play()
+    time.sleep(7)
+
+main()
+
+
